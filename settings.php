6d36b357ec06799e4c52eb1cd673d1e155282bda
lazzervan <84437576+lazzervan@users.noreply.github.com>
2023-04-17 20:29:53 +0800

    Update settings.php

--- settings.php
+++ settings.php
@@ -1,5 +1,5 @@
 <?php
- $con = new mysqli('localhost', 'root', '', 'main_db');
+ $con = new mysqli('us-cdbr-east-06.cleardb.net', 'b0f64541d7502d', '25dc2cd7', 'heroku_11249fee4af90ce');
 
 if($con->connect_errno > 0){
     die('Unable to connect to database [' . $con->connect_error . ']');
