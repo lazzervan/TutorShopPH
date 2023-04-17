6296e4ed26f48f35a315a72330a7b68e9f3dae65
lazzervan <84437576+lazzervan@users.noreply.github.com>
2023-04-17 21:19:07 +0800

    Update login.php

--- login.php
+++ login.php
@@ -1,6 +1,6 @@
 <?php
 
-$con = new mysqli('us-cdbr-east-06.cleardb.net', 'b0f64541d7502d', '25dc2cd7', 'heroku_11249fee4af90ce');
+ $con = new mysqli('us-cdbr-east-06.cleardb.net', 'b0f64541d7502d', '25dc2cd7', 'heroku_11249fee4af90ce');
 
 
 if($con->connect_errno > 0){
